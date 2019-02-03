854363c819c7756eeb3e294db50a910bf4cd267d
Sim Greenbaum <gbs4ever2@gmail.com>
2019-02-03 06:50:21 +0000

    Done.

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -2,6 +2,9 @@ class MusicLibraryController
 def initialize(path="./db/mp3s")
 MusicImporter.new(path).import
 end
+
+
+#missing command  for exit
     def call
     puts "Welcome to your music library!"
     puts"To list all of your songs, enter 'list songs'."
@@ -77,6 +80,6 @@ end
       end
 
 
-
+#missing command  for exit
 
 end
