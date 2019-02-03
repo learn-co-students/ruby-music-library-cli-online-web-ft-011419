5e3d564798d5b19598b1ef936b8d050ae7244b14
Jared Harbison <jared.harbison@gmail.com>
2019-02-03 21:33:37 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,33 @@
+class Genre 
+  extend Concerns::Findable
+  attr_accessor :name 
+  attr_reader :songs
+  @@all = []
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def self.destroy_all 
+    @@all.clear
+  end 
+  
+  def self.create(name)
+    genre = self.new(name)
+    genre.save
+    genre
+  end
+  
+  def initialize(name)
+    @name = name 
+    @songs = []
+  end 
+  
+  def save 
+    @@all << self 
+  end 
+  
+  def artists
+    self.songs.collect{|song| song.artist}.uniq
+  end
+end 
